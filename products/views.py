67c4ee5e5914705f50f24da344ac6da67ca24784
= <cavaleiromiau@gmail.com>
2025-09-19 22:39:07 -0300

    s

--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def see_products(request):
+    if request.method == 'GET':
+        name = 'Mary'
     return render(request, 'products/see_products.html')
 
 def inser_product(request):
