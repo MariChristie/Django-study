7b06e7e092144396a175c6d8ddfae9939ab90de8
= <cavaleiromiau@gmail.com>
2025-09-20 20:09:14 -0300

    update

--- products/views.py
+++ products/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import People
 
 def see_products(request):
-    if request.method == 'GET':
-        name = 'Mary'
-    return render(request, 'products/see_products.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
 
-def inser_product(request):
-    return HttpResponse("Insert a new product here.")
+        person = People(name=name, age=age)
+        person.save()
 
+        return redirect('see_products')
+
+    all_people = People.objects.all()
+    context = {'People': all_people}
+    return render(request, 'products/see_products.html', context)
+
+def insert_product(request):
+    return render(request, 'products/insert_product.html')
